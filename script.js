4d79f5ce1f13b23caec130d5190eb69cbf2fb115
Maureenm41 <maureenmrchl@gmail.com>
2021-02-10 09:59:21 +0100

    update hangman js

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-canvas
+/*canvas
 {
     width : 500;
     height : 600;
+}*/
+
+var guessWords = [
+    "JavaScript",
+    "Github",
+    "Visual Studio",
+   " Cat",
+    "Dog" ,
+    "Computer",
+    "Keyboard",
+    "Cellphone",
+    "Node.js",
+    "Among Us",
+]
+
+let answer ="";
+let maxWrong =5;
+let mistakes = 0;
+let guess = [];
+
+function randomWord()
+{
+    answer = guessWords[Math.floor(Math.random() * guessWords.length)];
 }
+
+function generateButton()
+{
+    let buttonHTML = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').map(letter =>);
+}
+
+
+randomWord();
